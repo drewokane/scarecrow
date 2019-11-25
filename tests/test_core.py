b8654d77cc1e3f3d4fbd5b981354068a231ba057
Drew O'Kane <drewokane@mlb-dokane.local>
2019-11-25 15:12:37 -0800

    Add weighted sampler plus tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,5 @@
 import numpy as np
-from itertools import repeat
+from itertools import repeat, chain
 
 from scarecrow import core
 
@@ -12,3 +12,21 @@ def test_simple_sampler():
     sample = ss.sample(5)
 
     assert len(list(sample)) == 5
+
+
+def test_weighted_sampler():
+    data = repeat(range(3), 5)
+    seed = 20191101
+    ws = core.WeightedSampler(data, seed=seed)
+
+    sample = list(ws.sample(5))
+
+    assert len(sample) == 5
+    assert sample[0] == tuple(range(3))
+
+    data = chain(repeat(range(3), 3), [[1, 1, 1]])
+    ws = core.WeightedSampler(data, seed=seed)
+
+    sample = list(ws.sample(10))
+
+    assert len(sample) == 10
