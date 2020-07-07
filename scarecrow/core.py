59be55a9d61e0328a8d23b459cb64e4c3026e343
Drew O'Kane <drew.okane@autodesk.com>
2020-07-07 10:35:38 -0700

    Simple dimensional sampler. More tests. Better abstraction for testing. New base class.

--- scarecrow/core.py
+++ scarecrow/core.py
@@ -1,16 +1,17 @@
 import random
 from collections import defaultdict
-from more_itertools import ilen, nth
+from copy import copy
+from more_itertools import ilen, nth, SequenceView
 from typing import Union, Sequence, Optional, Dict
 
 
-class SimpleSampler(object):
+class BaseSampler(object):
 
     def __init__(
-        self, 
+        self,
         data: Sequence[Sequence[Union[str, int, float]]],
-        seed: Optional[int] = None
-        ) -> None:
+        seed: Optional[int] = None,
+    ) -> None:
         """
         Initializes the class with the data to sample.
 
@@ -21,6 +22,12 @@ class SimpleSampler(object):
         self.data = list(data)
         self.len = ilen(self.data)
         self.rng = random.Random(x=seed)
+    
+    def sample(self, n: int):
+        return NotImplementedError('Sample method needs to be implemented!')
+
+
+class SimpleSampler(BaseSampler):
 
     def sample(self, n: int):
         for _ in range(n):
@@ -28,16 +35,14 @@ class SimpleSampler(object):
             yield nth(self.data, i)
 
 
-class WeightedSampler(object):
+class WeightedSampler(BaseSampler):
 
     def __init__(
             self,
             data: Sequence[Sequence[Union[str, int, float]]],
-            seed: Optional[int] = None
+            seed: Optional[int] = None,
     ) -> None:
-        self.data = list(data)
-        self.len = ilen(self.data)
-        self.rng = random.Random(x=seed)
+        super().__init__(data, seed=seed)
         self.hist = self._get_data_hist(self.data)
 
     def _make_hashable(self, thing):
@@ -60,3 +65,23 @@ class WeightedSampler(object):
         freqs = list(self.hist.values())
         for i in self.rng.choices(keys, weights=freqs, k=n):
             yield i
+
+
+class DimensionSampler(BaseSampler):
+
+    def __init__(
+        self,
+        data: Sequence[Sequence[Union[str, int, float]]],
+        seed: Optional[int] = None,
+        ) -> None:
+        super().__init__(data, seed=seed)
+        self.dims = self._make_dimensions()
+
+    
+    def _make_dimensions(self):
+        zipped = zip(*self.data)
+        return [col for col in zipped]
+    
+    def sample(self, n: int):
+        for _ in range(n):
+            yield [self.rng.choice(d) for d in self.dims]
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,32 +1,51 @@
 import numpy as np
+from functools import partial
 from itertools import repeat, chain
+from more_itertools import ilen, nth
 
 from scarecrow import core
 
 
-def test_simple_sampler():
+def test_simple_sampler(seed):
     data = repeat(range(3), 3)
-    seed = 10101515
     ss = core.SimpleSampler(data, seed=seed)
 
     sample = ss.sample(5)
 
-    assert len(list(sample)) == 5
+    assert ilen(sample) == 5
 
 
-def test_weighted_sampler():
+def test_weighted_sampler(seed):
     data = repeat(range(3), 5)
-    seed = 20191101
     ws = core.WeightedSampler(data, seed=seed)
 
     sample = list(ws.sample(5))
 
-    assert len(sample) == 5
-    assert sample[0] == tuple(range(3))
+    assert ilen(sample) == 5
+    assert nth(sample, 0) == tuple(range(3))
 
+    
+def test_weighted_sampler_weights_samples(seed):
     data = chain(repeat(range(3), 3), [[1, 1, 1]])
     ws = core.WeightedSampler(data, seed=seed)
 
     sample = list(ws.sample(10))
+    r_subset = lambda a: np.equal(a, range(3)).all()
+    one_subset = lambda a: np.equal(a, [1, 1, 1]).all()
 
-    assert len(sample) == 10
+    assert ilen(sample) == 10
+    assert ilen(filter(r_subset, sample)) >= 6
+    assert ilen(filter(one_subset, sample)) <= 4
+
+
+def test_dimension_sampler(seed):
+    data = chain(repeat(range(3), 3), [[1, 1, 1]])
+    ds = core.DimensionSampler(data, seed=seed)
+
+    sample = list(ds.sample(10))
+    print(f'Sample:\n{sample}')
+
+    assert ilen(sample) == 15
+    assert [0, 1, 1] in sample
+    assert [1, 1, 2] in sample
+    assert [0, 1, 2] in sample
